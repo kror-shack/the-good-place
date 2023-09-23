9f4c19f090fcd9d64e55ab014f3a4fe4ef63d32d
kror-shack <tariqfouzan1@gmail.com>
2023-09-23 16:18:16 +0300

    Integrate Redux Persist for state persistence.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ContactPage from "./pages/ContactPage/ContactPage";
 import FAQPage from "./pages/FaqPage/FaqPage";
 import MainPage from "./pages/MainPage/MainPage";
 import ReviewPage from "./pages/ReviewPage/ReviewPage";
-import store from "./store/store";
+import store, { persistor } from "./store/store";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import YourReservationsPage from "./pages/YourReservationsPage/YourReservationsPage";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
@@ -17,50 +17,53 @@ import MenuPage from "./pages/MenuPage/MenuPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import BookTablePage from "./pages/BookTablePage/BookTablePage";
 import ProtectedRoute from "./utils/auth/protectedRoute";
+import { PersistGate } from "redux-persist/integration/react";
 function App() {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/SignInPage" element={<SignInPage />} />
-          <Route path="/SignUpPage" element={<SignUpPage />} />
-          <Route path="/bookTablePage" element={<BookTablePage />} />
-          <Route
-            path="/YourReservationsPage"
-            element={
-              <ProtectedRoute>
-                <YourReservationsPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/orderDashboard" element={<OrderDashboard />} />
-          <Route
-            path="/checkoutPage"
-            element={
-              <ProtectedRoute>
-                <CheckoutPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/faqPage" element={<FAQPage />} />
-          <Route path="/contactPage" element={<ContactPage />} />
-          <Route path="/reviewPage" element={<ReviewPage />} />
-          <Route path="/menuPage" element={<MenuPage />} />
-          <Route
-            path="/profilePage"
-            element={
-              <ProtectedRoute>
-                <CheckoutPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-        {/* <Footer /> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/SignInPage" element={<SignInPage />} />
+            <Route path="/SignUpPage" element={<SignUpPage />} />
+            <Route path="/bookTablePage" element={<BookTablePage />} />
+            <Route
+              path="/YourReservationsPage"
+              element={
+                <ProtectedRoute>
+                  <YourReservationsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/orderDashboard" element={<OrderDashboard />} />
+            <Route
+              path="/checkoutPage"
+              element={
+                <ProtectedRoute>
+                  <CheckoutPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/faqPage" element={<FAQPage />} />
+            <Route path="/contactPage" element={<ContactPage />} />
+            <Route path="/reviewPage" element={<ReviewPage />} />
+            <Route path="/menuPage" element={<MenuPage />} />
+            <Route
+              path="/profilePage"
+              element={
+                <ProtectedRoute>
+                  <CheckoutPage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          {/* <Footer /> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,18 +5,30 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "../features/userSlice";
 import cartReducer from "../features/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
   reducer: {
-    rootReducer: rootReducer,
+    rootReducer: persistedReducer,
   },
+  middleware: [thunk],
 });
 
+export const persistor = persistStore(store);
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
